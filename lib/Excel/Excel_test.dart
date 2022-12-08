511de9e7c383a7114678165026ee6f4942464593
gehenna-yang <winer0707@naver.com>
2022-12-08 16:04:55 +0900

    sryang

--- lib/Excel/Excel_test.dart
+++ lib/Excel/Excel_test.dart
@@ -1,12 +1,16 @@
 
 
+import 'dart:convert';
 import 'dart:io';
 
+import 'package:excel/excel.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:syncfusion_flutter_xlsio/xlsio.dart' hide Column;
+import 'package:flutter/services.dart' show ByteData, rootBundle;
+import 'package:testfun_project/Excel/data_excel.dart';
 
 class CreateExcelWidget extends StatelessWidget {
   @override
@@ -32,6 +36,10 @@ class CreateExcelStatefulWidget extends StatefulWidget {
 
 class _CreateExcelState extends State<CreateExcelStatefulWidget> {
 
+  var excel;
+
+  List<String> sheetname_list = [];
+  List<int> rows_list = [];
 
   @override
   Widget build(BuildContext context) {
@@ -57,8 +65,44 @@ class _CreateExcelState extends State<CreateExcelStatefulWidget> {
     );
   }
 
+  Future<void> readExcel() async {
+    try {
+      Directory? directory = await getExternalStorageDirectory();
+      debugPrint('directory : $directory/${directory!.path}');
+
+      File pathfile = File(('${directory.path}/output_excel_test.xlsx'));
+
+      ByteData data = await rootBundle.load('${directory.path}/output_excel_test.xlsx');
+      print("file ? ${data.toString()}");
+      var bytes1 = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+      excel = Excel.decodeBytes(bytes1);
+
+      List<Data>? list = [];
+      for (var table in excel.tables.keys) {
+        print(table); //sheet Name
+        print(excel.tables[table]!.maxCols); // 가로 갯수
+        print(excel.tables[table]!.maxRows); // 세로 갯수
+        // for (var row in excel.tables[table]!.rows) {
+        //   print("$row"); // 가로에 나열된 값(리스트 형태)
+        // }
+        for (int i = 0; i<excel.tables[table]!.rows.length; i++) {
+          // list.addAll(excel.tables[table]!.row(i));
+
+          for(int j = 0; j<excel.tables[table]!.row(i).length; j++){
+            // print("??? ${excel.tables[table]!.row(i)}\n${excel.tables[table]!.row(i)[j]?.value}");
+            print("??? ${excel.tables[table]!.row(i)[j]?.value}");
+          }
+        }
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
   Future<void> generateExcel() async {
     //Create a Excel document.
+    Directory? directory = await getExternalStorageDirectory();
+    debugPrint('directory : $directory/${directory!.path}');
 
     //Creating a workbook.
     final Workbook workbook = Workbook();
@@ -69,21 +113,71 @@ class _CreateExcelState extends State<CreateExcelStatefulWidget> {
 
     // Enable calculation for worksheet.
     sheet.enableSheetCalculations();
-
     //Set data in the worksheet.
     sheet.getRangeByName('A1').columnWidth = 5;
     sheet.getRangeByName('B1').columnWidth = 15;
     sheet.getRangeByName('C1').columnWidth = 15;
     sheet.getRangeByName('D1').columnWidth = 15;
-    sheet.getRangeByName('E1').columnWidth = 7.50;
-    sheet.getRangeByName('F1').columnWidth = 9.73;
-    sheet.getRangeByName('G1').columnWidth = 8.82;
-    sheet.getRangeByName('H1').columnWidth = 4.46;
+    sheet.getRangeByName('E1').columnWidth = 15;
+    sheet.getRangeByName('F1').columnWidth = 15;
+    sheet.getRangeByName('G1').columnWidth = 15;
+    sheet.getRangeByName('H1').columnWidth = 15;
 
-    sheet.getRangeByName('B2').setText('test');
-    sheet.getRangeByIndex(2, 3).setText('excel');
-    sheet.getRangeByIndex(2, 4).setText('염병');
-    sheet.getRangeByIndex(3, 3).setText('c8');
+    for (var table in excel.tables.keys) {
+      print(table); //sheet Name
+      sheetname_list.add(table);
+      rows_list.add(excel.tables[table]!.maxRows);
+      print(excel.tables[table]!.maxCols); // 가로 갯수
+      print(excel.tables[table]!.maxRows); // 세로 갯수
+      // for (var row in excel.tables[table]!.rows) {
+      //   print("$row"); // 가로에 나열된 값(리스트 형태)
+      // }
+      for (int i = 0; i<excel.tables[table]!.rows.length; i++) {
+        for(int j = 0; j<excel.tables[table]!.row(i).length; j++){
+          print("??? ${excel.tables[table]!.row(i)[j]?.value}");
+          if(excel.tables[table]!.row(i)[j]?.value != null){
+            sheet.getRangeByIndex(i+1, j+1).setText(excel.tables[table]!.row(i)[j]?.value+"AA");
+            print("${i+1} / ${j+1} ${excel.tables[table]!.row(i)[j]?.value+"AA"}");
+          }
+        }
+      }
+    }
+
+    for(var r in rows_list){
+      for(int i = 0; i<5; i++){
+        for(int j = 0; j<8; j++){
+          sheet.getRangeByIndex(r+i+1, j+1).setText("TestExcel A${i+1}/${j+1}");
+          print("new data: TestExcel A${i+1}/${j+1}");
+        }
+      }
+    }
+
+    // sheet.getRangeByIndex(1, 2).setText('테스트1');
+    // sheet.getRangeByIndex(1, 3).setText('테스트2');
+    // sheet.getRangeByIndex(1, 4).setText('테스트3');
+    // sheet.getRangeByIndex(1, 5).setText('테스트4');
+    // sheet.getRangeByIndex(1, 6).setText('테스트5');
+    // sheet.getRangeByIndex(1, 7).setText('테스트6');
+    // sheet.getRangeByIndex(1, 8).setText('테스트7');
+    //
+    // sheet.getRangeByIndex(2, 1).setText('1');
+    // sheet.getRangeByIndex(2, 2).setText('데이터1');
+    // sheet.getRangeByIndex(2, 3).setText('데이터2');
+    // sheet.getRangeByIndex(2, 4).setText('데이터3');
+    // sheet.getRangeByIndex(2, 5).setText('데이터4');
+    // sheet.getRangeByIndex(2, 6).setText('데이터5');
+    // sheet.getRangeByIndex(2, 7).setText('데이터6');
+    // sheet.getRangeByIndex(2, 8).setText('데이터7');
+    //
+    // sheet.getRangeByIndex(3, 1).setText('2');
+    // sheet.getRangeByIndex(3, 2).setText('데이터8');
+    // sheet.getRangeByIndex(3, 3).setText('데이터9');
+    // sheet.getRangeByIndex(3, 4).setText('데이터10');
+    // sheet.getRangeByIndex(3, 5).setText('데이터11');
+    // sheet.getRangeByIndex(3, 6).setText('데이터12');
+    // sheet.getRangeByIndex(3, 7).setText('데이터13');
+    // sheet.getRangeByIndex(3, 8).setText('데이터14');
+
 
     //Save and launch the excel.
     final List<int> bytes = workbook.saveAsStream();
@@ -94,8 +188,6 @@ class _CreateExcelState extends State<CreateExcelStatefulWidget> {
     // await saveAndLaunchFile(bytes, 'Invoice.xlsx');
 
     // Directory directory = await getApplicationDocumentsDirectory();
-    Directory? directory = await getExternalStorageDirectory();
-    debugPrint('directory : $directory/${directory!.path}');
 
     if(directory != null){
       File(join('${directory.path}/output_excel_test.xlsx'))
@@ -110,6 +202,7 @@ class _CreateExcelState extends State<CreateExcelStatefulWidget> {
   void initState() {
     super.initState();
     _PermissionRequest();
+    readExcel();
   }
 
   _PermissionRequest() async {
---
+++ lib/Excel/data_excel.dart
@@ -0,0 +1,17 @@
+
+
+class data_excel {
+  data_excel({
+    this.num,
+    this.col,
+    this.row,
+    this.value,
+    this.sheet,
+  });
+
+  int? num;
+  int? col;
+  int? row;
+  String? value;
+  String? sheet;
+}
